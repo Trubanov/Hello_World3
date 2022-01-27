78dcbf65ab77dc0b2b58b19ba9475a9cb5125147
Gleb Trubanov <crew.gleb@gmail.com>
2022-01-27 17:18:17 +0300

    Initial Commit

---
+++ Hello_World3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello_World3
+//
+//  Created by Надежда Говор on 27.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
